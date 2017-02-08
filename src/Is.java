c6aac16e026f073171ff6fd64ea417adee193736
supernils <nils@manageall.se>
2017-02-08 14:38:13 +0100

    final

--- src/Is.java
+++ src/Is.java
@@ -1,6 +1,6 @@
 public final class Is {
 	
-	//Returns True if Argument is "y","Y","n" or "N"
+	//Returns true if arg is "y","Y","n" or "N"
 	public static Boolean YesNo(String arg){
 		if(arg.matches("^[yYnN]{1}")){
 			return true;
@@ -9,6 +9,7 @@ public final class Is {
 		}
 	}
 	
+	//Returns true if arg is a word without punctuations and spaces.
 	public static Boolean Word(String arg){
 		if(arg.matches("^[a-zA-ZÂ≈‰ƒˆ÷¸‹È…Ë»]*")){
 			return true;
@@ -17,6 +18,7 @@ public final class Is {
 		}	
 	}
 	
+	//Returns true if arg is a letter
 	public static Boolean Letter(String arg){
 		if(arg.matches("\\p{L}{1}")){
 			return true;
@@ -25,6 +27,7 @@ public final class Is {
 		}
 	}
 	
+	//Returns true id arg is a digit
 	public static Boolean Digit(String arg){
 		if(arg.length() == 1 && Is.Int(arg)){
 			return true;
@@ -33,6 +36,7 @@ public final class Is {
 		}
 	}
 	
+	//Returns true if arg is and integer
 	public static Boolean Int(String arg){
 		if(arg == arg.replaceAll("\\D","")){
 			return true;
@@ -41,6 +45,8 @@ public final class Is {
 		}
 	}
 	
+	//Returns true if arg is a double between floor and ceiling
+	//converts Integer input to double
 	public static Boolean DoubleBetween(String arg, double floor, double ceiling){
 		double dArg = Double.parseDouble(arg); // Not working
 		double dFloor = floor;
@@ -52,6 +58,7 @@ public final class Is {
 		}
 	}
 	
+	//Returns true if arg is a double between floor and ceiling
 	public static Boolean IntBetween(String arg, int floor, int ceiling){
 		if(Is.Int(arg) && Is.DoubleBetween(arg, floor, ceiling)){
 			return true;
